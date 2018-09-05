b6ad2abaad832b2d9bf341e426d6795cb59b22f2
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-05 10:29:56 +0200

    Folders helpers

---
+++ Source/Folders.js
@@ -0,0 +1,69 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import fs from 'fs-extra';
+import path from 'path';
+
+const _fileSystem = new WeakMap();
+
+/**
+ * Represents helpers for working with folders
+ */
+export class folders
+{
+    /**
+     * Initializes a new instance of {folders}
+     * @param {fs} fileSystem 
+     */
+    constructor(fileSystem) {
+        _fileSystem.set(this,fileSystem);
+    }
+
+    /**
+     * Copy one folder and its content recursively to a specified destination
+     * @param {string} destination Destination path to copy to
+     * @param {string} source Source path to copy from
+     */
+    copy(destination, source)
+    {
+        fs.copySync(source, destination);
+    }
+
+    /**
+     * Create a folder if it does not exist
+     * @param {string} path Name of the folder to make sure exists
+     */
+    makeFolderIfNotExists(path)
+    {
+        var dir = path;
+
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+    }
+
+    /**
+     * Search for a specific file pattern within a folder, recursively
+     * @param {string} folder Folder to search from
+     * @param {string} pattern Pattern of files to look for
+     */
+    searchRecursive(folder, pattern) {
+        var results = [];
+
+        fs.readdirSync(folder).forEach(function (dirInner) {
+            dirInner = path.resolve(folder, dirInner);
+            var stat = fs.statSync(dirInner);
+
+            if (stat.isDirectory()) {
+                results = results.concat(folderHandler.SearchRecursive(dirInner, pattern));
+            }
+
+            if (stat.isFile() && dirInner.endsWith(pattern)) {
+                results.push(dirInner);
+            }
+        });
+
+        return results;
+    };
+}
