6a55d6a5f0bbb0cb6904868e79af87d7110db273
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-11 14:59:10 +0200

    Doing a first run check and missing boilerplates

--- Source/dolittle.js
+++ Source/dolittle.js
@@ -9,6 +9,18 @@ args
     .command('cluster', 'Work with cluster hosting Dolittle')
     .command('create', 'Create something from one of the boilerplates');
 
-args.parse(process.argv);
+args.parse(process.argv);    
+let showHelpIfNeeded = () => {
+    if( !args.sub.length ) args.showHelp();
+}
 
-if( !args.sub.length ) args.showHelp();
+if( global.configManager.isFirstRun || !global.boilerPlatesManager.hasBoilerPlates ) {
+    if( global.configManager.isFirstRun ) global.logger.info('This is the first time you run this tool, hang on tight while we get it ready');
+    else global.logger.info('There are no boiler plates, hang on tight while we get it ready');
+    global.boilerPlatesManager.update().then(() => {
+        console.log('\n');
+        showHelpIfNeeded();
+    });
+} else {
+    showHelpIfNeeded();
+}
