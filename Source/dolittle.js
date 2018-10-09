347e70ed53a045baa8a19854306521004f6acb0b
Sindre Wilting <sindrewilting@gmail.com>
2018-10-09 20:29:05 +0200

    Fixes so that it doesn't try to fetch the github repos twice when using dolittle update the first time

--- Source/dolittle.js
+++ Source/dolittle.js
@@ -7,10 +7,13 @@ import global from './global';
 
 let pkg = require('../package.json');
 console.log(`Dolittle CLI v${pkg.version}\n`);
-
+let updating = false;
 args
     .command('update', 'Update all artifacts', () => {
-        global.boilerPlatesManager.update();
+        updating = true;
+        global.boilerPlatesManager.update().then(() => {
+            return;
+        });
     })
     .command('cluster', 'Work with cluster hosting Dolittle')
     .command('create', 'Create something from one of the boilerplates')
@@ -21,7 +24,9 @@ let showHelpIfNeeded = () => {
     if( !args.sub.length ) args.showHelp();
 }
 
-if( global.configManager.isFirstRun || !global.boilerPlatesManager.hasBoilerPlates ) {
+if ( !updating && 
+    (global.configManager.isFirstRun || !global.boilerPlatesManager.hasBoilerPlates)
+    ) {
     if( global.configManager.isFirstRun ) global.logger.info('This is the first time you run this tool, hang on tight while we get it ready');
     else global.logger.info('There are no boiler plates, hang on tight while we get it ready');
     global.boilerPlatesManager.update().then(() => {
