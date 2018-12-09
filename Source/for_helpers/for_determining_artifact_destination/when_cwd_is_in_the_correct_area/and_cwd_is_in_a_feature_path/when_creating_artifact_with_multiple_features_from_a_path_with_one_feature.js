5b1a718a3efd57ae32678ec2e1b6e462cb9e9ee7
Sindre Wilting <sindrewilting@gmail.com>
2018-12-09 11:40:31 +0100

    Change request

--- Source/for_helpers/for_determining_artifact_destination/when_cwd_is_in_the_correct_area/and_cwd_is_in_a_feature_path/when_creating_artifact_with_multiple_features_from_a_path_with_one_feature.js
+++ Source/for_helpers/for_determining_artifact_destination/when_cwd_is_in_the_correct_area/and_cwd_is_in_a_feature_path/when_creating_artifact_with_multiple_features_from_a_path_with_one_feature.js
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 import {determineDestination} from '../../../../helpers';
 import { path_with_one_feature } from './given/path_with_one_feature';
-import { path_with_multiple_features } from './given/path_with_multiple_features';
 
 const path = require('path');
 
@@ -14,17 +13,6 @@ describe('when creating artifact with multiple features from a path with one fea
     let featureSegments = 'feature2.feature3';
     (beforeEach => {
         context = new path_with_one_feature();
-        result = determineDestination(context.area, context.language, `${featureSegments}.${context.name}`, context.cwd, context.boundedContextPath, context.dolittleConfig);
-    })();
-    it('Should determine the correct destination', () => result.destination.should.equal(path.join(context.boundedContextRoot, context.dolittleConfig[context.language][context.area], context.featureSegments.split('.').join(path.sep), featureSegments.split('.').join(path.sep))));
-});
-
-describe('when creating artifact with multiple features from a path with multiple features', () => {
-    let context = null;
-    let result = '';
-    let featureSegments = 'feature3.feature4';
-    (beforeEach => {
-        context = new path_with_multiple_features();
         result = determineDestination(context.area, context.language, `${featureSegments}.${context.name}`, context.cwd, context.boundedContextPath, context.dolittleConfig);
     })();
     it('Should determine the correct destination', () => result.destination.should.equal(path.join(context.boundedContextRoot, context.dolittleConfig[context.language][context.area], context.featureSegments.split('.').join(path.sep), featureSegments.split('.').join(path.sep))));
--- Source/for_helpers/for_determining_artifact_destination/when_cwd_is_in_the_correct_area/and_cwd_is_in_a_feature_path/when_creating_artifact_without_a_feature_from_a_path_with_one_feature.js
+++ Source/for_helpers/for_determining_artifact_destination/when_cwd_is_in_the_correct_area/and_cwd_is_in_a_feature_path/when_creating_artifact_without_a_feature_from_a_path_with_one_feature.js
@@ -5,7 +5,6 @@
 
 import { path_with_one_feature } from './given/path_with_one_feature';
 import { determineDestination } from '../../../../helpers';
-import { path_with_multiple_features } from './given/path_with_multiple_features';
 const path = require('path');
 
 describe('when creating artifact without a feature from a path with one feature', () => {
@@ -13,16 +12,6 @@ describe('when creating artifact without a feature from a path with one feature'
     let result = '';
     (beforeEach => {
         context = new path_with_one_feature();
-        result = determineDestination(context.area, context.language, context.name, context.cwd, context.boundedContextPath, context.dolittleConfig);
-    })();
-    it('Should determine the correct destination', () => result.destination.should.equal(path.join(context.boundedContextRoot, context.dolittleConfig[context.language][context.area], context.featureSegments.split('.').join(path.sep) + path.sep)));
-});
-
-describe('when creating artifact without a feature from a path with multiple features', () => {
-    let context = null;
-    let result = '';
-    (beforeEach => {
-        context = new path_with_multiple_features();
         result = determineDestination(context.area, context.language, context.name, context.cwd, context.boundedContextPath, context.dolittleConfig);
     })();
     it('Should determine the correct destination', () => result.destination.should.equal(path.join(context.boundedContextRoot, context.dolittleConfig[context.language][context.area], context.featureSegments.split('.').join(path.sep) + path.sep)));
