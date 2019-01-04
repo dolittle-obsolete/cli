43362cd0a8e2f10d4904cbd682dfcd73445ccc4c
Sindre Wilting <sindrewilting@gmail.com>
2019-01-04 13:42:21 +0100

    Create a single entry point for handling 'dolittle add <artifact>' commands

--- Source/CommandManager.js
+++ Source/CommandManager.js
@@ -3,9 +3,9 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import {InquirerManager} from './artifacts/InquirerManager';
-import { Application, BoundedContext } from '@dolittle/tooling.common';
-import {  determineDestination } from './helpers';
+import { Application, BoundedContext, ArtifactTemplate, helpers } from '@dolittle/tooling.common';
+import {  determineDestination, requireBoundedContext, contextFromArgs, getArtifactArgumentDependencies, createUsageTextForArtifact, showHelpIfNeeded, requireArtifactTemplate } from './helpers';
+import { usagePrefix } from '../bin/helpers';
 
 /**
  * Represents a manager for artifacts
@@ -28,12 +28,52 @@ export class CommandManager {
     }
     /**
      * Handles an 'dolittle add <artifact>' command
-     * @param {any} context CLI arguments given in context of the dependencies
-     * @param {BoundedContext} boundedContext
+     * @param {import('args')} args
+     * @param {string} commandName The actual command name. 'dolittle add <commandName>'
+     * @param {string} artifactType The artifactType
+     * @param {string} description The args description text
+     * @param {string} cwd The current working directory
      * @memberof CommandManager
      */
-    handleAddArtifact(context, boundedContext) {
-        this.#logger.info(`Handling artifact. args: ${args}. Bounded Context Language: ${boundedContext.core.language}`);
+    handleAddArtifact(args, commandName, artifactType, description, cwd) {
+        let boundedContext = requireBoundedContext(this.#boundedContextsManager, cwd, this.#logger);
+        if (!boundedContext) process.exit(1);
+        
+        let artifactTemplate = requireArtifactTemplate(this.#artifactsManager, 'csharp', artifactType, this.#logger); // Hard coded language, for now
+        if (!artifactTemplate) process.exit(1);
+        
+        let dependencies = getArtifactArgumentDependencies(this.#artifactsManager, artifactType, boundedContext);
+        const USAGE = createUsageTextForArtifact(dependencies, artifactType);
+
+        args
+            .example(USAGE, description);
+        args.parse(process.argv, {value: usagePrefix + USAGE, name: `dolittle add ${commandName}`});
+
+        showHelpIfNeeded(args, dependencies.length);
+
+        
+        let context = contextFromArgs(args.sub, dependencies);
+
+        // Lets assume context has 'name' and determine the destintation folder based upon a '.' seperated name and the dolittlerc configuration
+        /**
+         * @type {{destination: string, name: string}} The destination path and the actual name of the artifact
+         */
+        let destinationAndName = helpers.determineDestination(artifactTemplate.area, artifactTemplate.language, context['name'], cwd, boundedContext.path, this.#dolittleConfig);
+        context['name'] = destinationAndName.name;
+        this.addArtifact(context, artifactTemplate, destinationAndName.destination);
+    }
+
+    /**
+     * Adds an artifact based on the given context
+     *
+     * @param {any} context
+     * @param {ArtifactTemplate} artifactTemplate
+     * @param {string} destinationFolder
+     * @memberof CommandManager
+     */
+    addArtifact(context, artifactTemplate, destinationFolder) {
+        this.#logger.info(context);
+        this.#logger.info(`Creating artifact with artifacttype '${artifactTemplate.type}', language '${artifactTemplate.language}', name '${context['name']} and destination folder '${destinationFolder}'`);
     }
     /**
      * Handles the 'dolittle create application' command
