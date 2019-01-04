faa8aacab96e8386f3bf0c9744c621d2c9df3ea0
Sindre Wilting <sindrewilting@gmail.com>
2019-01-04 10:41:51 +0100

    remove common stuff from helpers and add something useful

--- Source/helpers.js
+++ Source/helpers.js
@@ -2,54 +2,64 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+import {ApplicationsManager, ArtifactsManager, BoundedContext, BoundedContextsManager, Dependency} from '@dolittle/tooling.common';
 /**
  * The usage prefix used in commands info
  * @returns {string} the usage prefix
  */
 export const usagePrefix = '\n\t ';
 
+/**
+ * Show command help if needed
+ *
+ * @export
+ * @param {import('args')} args
+ * @param {*} numDependencies
+ */
+export function showHelpIfNeeded(args, numDependencies) {
+    if (numDependencies > 0 && (! args.sub.length || args.sub.length !== numDependencies)) args.showHelp();
+}
+/**
+ * Creates a context object from the argument list and the dependencies. Assumes that the arguments are given in the same order as the dependencies
+ *
+ * @export
+ * @param {string[]} args
+ * @param {Dependency[]} dependencies
+ */
+export function contextFromArgs(args, dependencies) {
+    if (args.length !== dependencies.length) throw new Error('Args does not match dependencies');
+    let context = {};
+    dependencies.forEach((dep, i) => {
+        context[dep.name] = args[i];        
+    });
+    return context;
+}   
+/**
+ * Creates a part of the usage text that describes the command argument inputs 
+ *
+ * @export
+ * @param {Dependency[]} argumentDependecies
+ * @returns {string}
+ */
+export function createUsageArgumentText(argumentDependecies) {
+    return argumentDependecies.map(_ => {
+        return _.description === undefined || _.description === '' ? 
+            `[${_.name}]`
+            : `[${_.name} - ${_.description}]`;
+    }).join(' ');
+}
+/**
+ * Creates the USAGE text for 'dolittle add <artifact>' commands
+ *
+ * @export
+ * @param {Dependency[]} argumentDependencies
+ * @param {string} artifactType
+ * @returns
+ */
+export function createUsageTextForArtifact(argumentDependencies, artifactType) {
+    return `dolittle add ${artifactType} ${createUsageArgumentText(argumentDependencies)}` ;
+}
 
-/**
- * Gets the full directory path
- * @param {string} filePath
- * @returns {string} directory path
- */
-export function getFileDirPath(filePath) {
-    const path = require('path');
-    filePath = path.normalize(filePath);
-    return path.parse(filePath).dir;
-}
-/**
- * Gets the filename without extension
- * @param {string} filePath
- * @returns {string} filename
- */
-export function getFileName(filePath) {
-    const path = require('path');
-    filePath = path.normalize(filePath);
-    return path.parse(filePath).name;
-}
-/**
- * Gets the filename with extension
- * @param {string} filePath
- * @returns {string} filename
- */
-export function getFileNameAndExtension(filePath) {
-    const path = require('path');
-    filePath = path.normalize(filePath);
-    return path.parse(filePath).base;
-}
-/**
-  * Gets the directory name
-  * @param {string} filePath
-  * @returns {string} file dirname
-  */
-export function getFileDir(filePath) {
-    const path = require('path');
-    filePath = path.normalize(filePath);
-    return path.dirname(filePath);
-}
 /**
  * Validate the name argument
  * @param {string} name 
@@ -70,39 +80,37 @@ export function validateArgsNameInput(name) {
     }
 }
 /**
+ * Gets all userinput dependencies with 'type': "argument" for applications
  *
- *
- * @param {string} area
- * @param {string} language
- * @param {string} name
- * @param {string} cwd
- * @param {string} boundedContextPath
- * @param {any} dolittleConfig
- * 
- * @returns {{destination: string, name: string}}
+ * @export
+ * @param {ApplicationsManager} applicationsManager
+ * @param {string} [language='any']
+ * @returns {Dependency[]}
  */
-export function determineDestination(area, language, name, cwd, boundedContextPath, dolittleConfig){
-    const path = require('path');
-    let config = dolittleConfig[language];
-    if (config === undefined || config === null)
-        throw `No configuration for language ${language}`;
-    const areaName = config[area];
-    if (areaName === undefined || areaName === null)
-        throw `No configuration for area ${area} for language ${language}`;
-    const boundedContextRoot = path.dirname(boundedContextPath);
-    const regExp = new RegExp(
-        `(${escapeRegex(boundedContextRoot)})` + // Match first part of path (root of bounded-context) 
-        `(?:${escapeRegex(path.sep)}[^${escapeRegex(path.sep)}]+)?` + // Non-matching group matching the segment after the bounded-context root folder. This indicates the area of the artifact
-        `(${escapeRegex(path.sep)}?.*)` // Match all the segments after the area
-        
-    );
-    const newDestination = cwd.replace(regExp, '$1' + path.sep + areaName + '$2');
-
-    let splittedName = name.split('.');
-    const featurePath = path.sep + splittedName.slice(0, -1).join(path.sep);
-    return {destination: newDestination + featurePath, name: splittedName[splittedName.length - 1]};
+export function getApplicationArgumentDependencies(applicationsManager, language = 'any') {
+    return applicationsManager.getDependencies(language).filter(_ => _.userInputType !== undefined && _.userInputType === 'argument');
+}
+/**
+ * Gets all userinput dependencies with 'type': "argument" for bounded contexts
+ *
+ * @export
+ * @param {BoundedContextsManager} boundedContextsManager
+ * @param {string} [language='any']
+ * @returns {Dependency[]}
+ */
+export function getBoundedContextsArgumentDependencies(boundedContextsManager, language = 'any') {
+    return boundedContextsManager.getDependencies(language).filter(_ => _.userInputType !== undefined && _.userInputType === 'argument');
 }
 
-function escapeRegex(s) {
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+/**
+ * Gets all userinput dependencies with 'type': "argument" for artifacts
+ *
+ * @export
+ * @param {ArtifactsManager} artifactsManager
+ * @param {string} artifactType
+ * @param {BoundedContext} boundedContext
+ * @returns {Dependency[]}
+ */
+export function getArtifactArgumentDependencies(artifactsManager, artifactType, boundedContext) {
+    return artifactsManager.getDependencies(artifactType, boundedContext.core.language).filter(_ => _.userInputType !== undefined && _.userInputType === 'argument');
 }
