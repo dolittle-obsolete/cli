3f2b26ad8c7a3690d1741781a7deae3147e62554
Sindre Wilting <sindrewilting@gmail.com>
2019-02-14 09:10:37 +0100

    Re-formating the console output for boilerplates check

--- Source/dolittle-boilerplates-check.js
+++ Source/dolittle-boilerplates-check.js
@@ -9,6 +9,12 @@ import args from 'args';
 import globals from './globals';
 import {boilerPlatesManager} from '@dolittle/tooling.common';
 import semver from 'semver';
+import { usagePrefix } from './helpers';
+
+const USAGE = 'dolittle boilerplates check';
+args
+    .example(USAGE, 'Checks for out-of-date boilerplates installed on the system');
+args.parse(process.argv, {value: usagePrefix + USAGE, name: 'dolittle boilerplates check'});
 
 console.log('Checking versions:\n');
 let paths = boilerPlatesManager.installedBoilerplatePaths;
@@ -20,10 +26,22 @@ paths.map(path => JSON.parse(require('fs').readFileSync(require('path').join(pat
 
 boilerPlatesManager.getLatestBoilerplateVersion(...locallyInstalled.map(pkg => pkg.name))
     .then(latestVersions => {
+        let anyOutOfDate = false;
         locallyInstalled.forEach((pkg, i) => {
             if (semver.gt(latestVersions[i], pkg.version)) {
-                console.log(`${pkg.name} is outdated.\n$ npm i -g ${pkg.name}\n`);
+                locallyInstalled[i].outOfDate = true;
+                anyOutOfDate = true;
             }
         });
+
+        if (anyOutOfDate) {
+            let outOfDatePackages = locallyInstalled.map((pkg, i) => new Object({outOfDate: pkg.outOfDate, name: pkg.name, version: pkg.version, latest: latestVersions[i]})).filter(pkg => pkg.outOfDate);
+            console.log(`There are ${outOfDatePackages.length} out-of-date packages:`);
+            outOfDatePackages.forEach(pkg => console.log(`${pkg.name}: ${pkg.version} ==> ${pkg.latest}`));
+            console.log();
+            console.log('Update all packages with');
+            console.log(`$ npm i -g ${outOfDatePackages.map(_ => _.name).join(' ')}`);
+        } 
+        else console.log('All boilerplates are up-to-date');
     });
 
