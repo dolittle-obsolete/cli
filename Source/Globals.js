1b93f27387c9770652fcd9326b5e41d7756a964f
Sindre Wilting <sindrewilting@gmail.com>
2019-02-25 08:08:38 +0100

    Initialize globals to get managers

--- Source/Globals.js
+++ Source/Globals.js
@@ -3,12 +3,23 @@
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+import {boilerplatesConfig, projectConfig, getManagers} from '@dolittle/tooling.common'
 import { CommandManager } from './Commands/CommandManager';
 import { Inquirer } from './Inquirer';
-import {boilerplatesConfig, projectConfig, getManagers} from '@dolittle/tooling.common'
+import { CliContext } from './CliContext';
+import outputter from './Outputter';
+
+/**
+ * Initializes the CLI program
+ *
+ * @class Globals
+ */
 class Globals {
+    isInitialized = false;
+    
     #projectConfig;
     #boilerplatesConfig;
+
     #inquirer;
     #commandManager;
 
@@ -28,16 +39,6 @@ class Globals {
         this.#installHandlers();
         this.#projectConfig = projectConfig;
         this.#boilerplatesConfig = boilerplatesConfig;
-
-        // this.#inquirer = new Inquirer();
-        // this.#commandManager = new CommandManager();
-    }
-
-    get inquirer() {
-        return this.#inquirer;
-    }
-    get commandManager() {
-        return this.#commandManager;
     }
     get projectConfig() {
         return this.#projectConfig;
@@ -49,16 +50,65 @@ class Globals {
         if (!this.#boilerplatesManager) this.init();
         return this.#boilerplatesManager;
     }
+    get applicationsManager() {
+        if (!this.#applicationsManager) this.init();
+        return this.#applicationsManager;
+    }
+    get artifactsManager() {
+        if (!this.#artifactsManager) this.init();
+        return this.#artifactsManager;
+    }
+    get boundedContextsManager() {
+        if (!this.#boundedContextsManager) this.init();
+        return this.#boundedContextsManager;
+    }
+    get dependenciesManager() {
+        if (!this.#dependenciesManager) this.init();
+        return this.#dependenciesManager;
+    }
+    get inquirer() {
+        if (!this.#inquirer) this.init();
+        return this.#inquirer;
+    }
+    get commandManager() {
+        if (!this.#commandManager) this.init();
+        return this.#commandManager;
+    }
+    get cliContext() {
+        if (!this.#cliContext) this.init();
+        return this.#cliContext;
+    }
 
     init() {
+        if (this.isInitialized) return;
+
+        let spinner = outputter.spinner('Starting up tooling CLI').start();
         const managers = getManagers();
         this.#boilerplatesManager = managers.boilerplatesManager;
+        this.#applicationsManager = managers.applicationsManager;
+        this.#artifactsManager = managers.artifactsManager;
+        this.#boundedContextsManager = managers.boundedContextsManager;
+        this.#dependenciesManager = managers.dependenciesManager;
+
+        this.#inquirer = new Inquirer(this.#dependenciesManager);
+        this.#commandManager = new CommandManager(managers.boilerplatesManager, managers.applicationsManager, managers.boundedContextsManager, managers.artifactsManager, managers.dependenciesManager);
+        this.#cliContext = new CliContext(outputter, this.#projectConfig, 
+            {
+                boilerplatesManager: managers.boilerplatesManager,
+                applicationsManager: managers.applicationsManager,
+                boundedContextsManager: managers.boundedContextsManager,
+                artifactsManager: managers.artifactsManager,
+                dependenciesManager: managers.dependenciesManager,
+                commandManager: this.#commandManager
+            }, this.#inquirer);
         
+        spinner.stop();
     }
     
     #installHandlers() {
         process.on('unhandledRejection', (reason, _) => {
-            console.error('Unhandled rejection:', reason.stack)
+            outputter.error(error, 'Unhandled rejection:', reason.stack);
+            process.exit(1);
         });
     }
     
