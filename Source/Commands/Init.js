993556be2b7f63159e120eb748b3c0bc1273536b
Sindre Wilting <sindrewilting@gmail.com>
2019-02-27 08:22:37 +0100

    Start on init

--- Source/Commands/Init.js
+++ Source/Commands/Init.js
@@ -4,11 +4,24 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { Command } from './Command';
-
+import { CliContext } from '../CliContext';
+import { ParserResult } from '../ParserResult';
 class Init extends Command{
     constructor() {
         super('init', 'Initializes the Dolittle CLI', 
             'dolittle init', undefined, '', 'Sets up the environment for the CLI' );
+    }
+    /**
+     * @inheritdoc
+     * @param {ParserResult} parserResult
+     * @param {CliContext} context
+     */
+    async action(parserResult, context) {
+        if (parserResult.help) {
+            context.outputter.print(this.helpDocs);
+            return;
+        }
+        
     }
 }
 
