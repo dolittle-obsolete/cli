274b53cd3419513404a026ef7ef1e38d7d9a348d
Sindre Wilting <sindrewilting@gmail.com>
2019-04-05 17:26:54 +0200

    docs

--- Source/Actions/Boilerplates/initBoilerplates.js
+++ Source/Actions/Boilerplates/initBoilerplates.js
@@ -1,3 +1,6 @@
+import { Outputter } from "../../Outputter";
+import { BoilerplatesManager } from "@dolittle/tooling.common/dist/boilerplates/BoilerplatesManager";
+
 /*---------------------------------------------------------------------------------------------
 *  Copyright (c) Dolittle. All rights reserved.
 *  Licensed under the MIT License. See LICENSE in the project root for license information.
@@ -6,6 +9,7 @@
 /**
  * Initializes the boilerplates system in the common tooling
  * @param {Outputter} outputter
+ * @param {BoilerplatesManager} boilerplatesManager 
  */
 export default async function initBoilerplates(outputter, boilerplatesManager) {
     let spinner = outputter.spinner('Initializing boilerplates system').start();
