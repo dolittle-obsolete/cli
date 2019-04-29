ab1a58213e66682a96cb2127a642b12d175395a6
Sindre Wilting <sindrewilting@gmail.com>
2019-04-29 12:46:08 +0200

    Add Command group initial implementation

--- Source/Commands/Groups/Add/AddCommand.js
+++ Source/Commands/Groups/Add/AddCommand.js
@@ -8,6 +8,10 @@ import { group } from './Add';
 import { ParserResult } from '../../../ParserResult';
 import { CliContext } from '../../../CliContext';
 import { ArtifactTemplate } from '@dolittle/tooling.common/dist/artifacts/ArtifactTemplate';
+import seperateDependencies from '../../../Util/seperateDependencies';
+import requireArguments from '../../../Util/requireArguments';
+import resolveAllDependencies from '../../../Util/resolveAllDependencies';
+import chooseTemplate from '../../../Actions/Add/chooseTemplate';
 
 export class AddCommand extends Command {
     #_artifactTemplates;
@@ -18,7 +22,7 @@ export class AddCommand extends Command {
      * @memberof Installed
      */
     constructor(artifactType, artifactTemplates) {
-        super(artifactType, 'Something', 'something', group);
+        super(artifactType, artifactTemplates[0].description, 'dolittle add ' + artifactType, group);
         this.#_artifactTemplates = artifactTemplates;
     }
     /**
@@ -40,6 +44,27 @@ export class AddCommand extends Command {
             context.outputter.print(this.helpDocs);
             return;
         }
-        
+        let projectConfigObj = context.projectConfig.store;
+        let language = projectConfigObj.coreLanguage;
+        /**
+         * @type {ArtifactTemplate[]}
+         */
+        let templatesWithLanguage = this.artifactTemplates.filter(_ => _.boilerplate.language === language);
+
+        if (!templatesWithLanguage.length || templatesWithLanguage.length < 1) {
+            context.outputter.warn(`There are no artifact templates of type '${this.name}' with language '${language}'.`);
+            return;
+        }
+
+        let args = [parserResult.firstArg, ...parserResult.restArgs, ...parserResult.extraArgs];
+        requireArguments(this, context.outputter, args, 'Missing artifact name');
+
+        let template = templatesWithLanguage[0];
+
+        if (templatesWithLanguage.length > 1) template = await chooseTemplate(); 
+
+        let dependencies = seperateDependencies(template.allDependencies());
+        let boilerplateContext = await resolveAllDependencies(context.managers.dependenciesManager, context.inquirer, template.boilerplate, context.cwd, args, dependencies);
+        context.managers.artifactsManager.createArtifact(boilerplateContext, template, context.cwd);
     }
 }
