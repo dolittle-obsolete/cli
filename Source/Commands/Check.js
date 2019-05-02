1d76fd3566ca9cba49f79645378300b41d3978bb
Sindre Wilting <sindrewilting@gmail.com>
2019-05-02 09:37:54 +0200

    dolittle check asks user to update CLI if newer is found

--- Source/Commands/Check.js
+++ Source/Commands/Check.js
@@ -6,17 +6,19 @@
 import { Command } from './Command';
 import { CliContext } from '../CliContext';
 import { ParserResult } from '../ParserResult';
-import requireInternet from '../Util/requireInternet';
 import { getLatestVersion, isCompatibleUpgrade, isGreaterVersion } from '../Util/packageVersion';
-import chalk from 'chalk';
 import { Outputter } from '../Outputter';
+import spawn from 'cross-spawn';
+import inquirer from 'inquirer';
+import requireInternet from '../Util/requireInternet';
+import chalk from 'chalk';
 
 const pkg = require('../../package.json');
 const description = `Checks the Dolittle CLI by comparing the version of the CLI tool with the version tagged with 'latest' on NPM.
 The version text is color coded:
-    Red:    There is a major/breaking change update to the CLI.
-    Yellow: There is a compatible upgrade to the CLI.
-    Green:  The version of the CLI is greater or equal to the version tagged with 'latest' on NPM.
+    ${chalk.red('Red')}:    There is a major/breaking change update to the CLI.
+    ${chalk.yellow('Yellow')}: There is a compatible upgrade to the CLI.
+    ${chalk.green('Green')}:  The version of the CLI is greater or equal to the version tagged with 'latest' on NPM.
 `;
 class Check extends Command {
     constructor() {
@@ -41,7 +43,13 @@ class Check extends Command {
         const compatibleUpgrade = isCompatibleUpgrade(latestVersion, currentVersion);
         const majorUpgrade = isGreaterVersion(latestVersion, currentVersion);
         this.output(context.outputter, pkgName, currentVersion, latestVersion, sameVersion, compatibleUpgrade, majorUpgrade);
-
+        
+        if (compatibleUpgrade || majorUpgrade) {
+            
+            const update = await this.#askWhetherToUpdate(majorUpgrade);
+            if (update)
+                spawn('npm', ['i', '-g', `${pkgName}@${latestVersion}`], {cwd: process.cwd(), stdio: 'inherit'});
+        }
     }
     /**
      *
@@ -76,6 +84,19 @@ class Check extends Command {
             outputter.print(`${pkgName} ${versionText} - You're using the version tagged with 'latest'`);
         else
             outputter.print(`${pkgName} ${versionText} --> ${latestVersionText}`);
+    }
+    /**
+     * Asks the user whether or not the update the tooling
+     * @param {boolean} majorUpgrade Whether there is a major upgrade or not
+     */
+    async #askWhetherToUpdate(majorUpgrade) {
+        let answers = await inquirer.prompt([
+            {
+                type: 'confirm', default: false, name: 'update', 
+                message: `There is a ${majorUpgrade? 'major update' : 'minor update'} to the CLI. Do you wish to update to latest?`
+            }
+        ]);
+        return answers['update'];
     }
 }
 
