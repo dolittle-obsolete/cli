e80c62d9a1cd94aefdfc14c105e60eb5c01a4f5b
Sindre Wilting <sindrewilting@gmail.com>
2019-06-27 23:18:11 +0200

    Fix naming

---
+++ Source/Commands/Command.ts
@@ -0,0 +1,110 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Command as BaseCommand, ICommand } from '@dolittle/tooling.common.commands';
+import { IDependency, dependencyIsPromptDependency, argumentUserInputType, IPromptDependency, IDependencyResolvers } from '@dolittle/tooling.common.dependencies';
+import { IBusyIndicator, ICanOutputMessages } from '@dolittle/tooling.common.utilities';
+import chalk from 'chalk';
+import { Outputter } from '../Outputter';
+import { BusyIndicator } from '../BusyIndicator';
+import hasHelpOption from '../Util/hasHelpOption';
+
+/**
+ * The base class of a {Command} that is wrapped to fit the needs of the CLI. 
+ *
+ * @export
+ * @class Command
+ */
+export class Command extends BaseCommand {
+    
+    static fromCommand(command: ICommand, commandGroup?: string, namespace?: string) {
+        const usage = `dolittle${namespace? ' ' + namespace : ''}${commandGroup? ' ' + commandGroup : ''} ${command.name}`;
+        return new Command(command.name, command.description, usage, commandGroup, undefined, command.shortDescription, command.dependencies, command);
+    }
+ 
+    /**
+     * The usage text used 
+     *
+     * @type {string}
+     */
+    usage: string;
+    
+    /**
+     * The group of commands the command belongs to, if any
+     *
+     * 
+     * @type {string}
+     */
+    readonly group?: string
+
+    /**
+     * The optional help text that is printed as a addition to the description and the usage
+     *
+     * 
+     * @type {string}
+     */
+    help?: string
+
+    /**
+     * Creates an instance of {Command}.
+     * @param {string} name
+     * @param {string} description
+     * @param {string} usage
+     * @param {string?} group
+     * @param {string?} help
+     * @param {string[]?} args
+     * @memberof Command
+     */
+    constructor(name: string, description: string, usage: string, group?: string, help?: string, shortDescription?: string, dependencies?: IDependency[],
+        private _derivedCommand?: ICommand) {
+        super(name, description, false, shortDescription, dependencies)
+        this.usage = usage;
+        this.group = group;
+        this.help = help;
+    }
+
+    async action(dependencyResolvers: IDependencyResolvers, currentWorkingDirectory: string, coreLanguage: string, commandArguments: string[], commandOptions: Map<string, any>, namespace?: string,
+            outputter: ICanOutputMessages = new Outputter(), busyIndicator: IBusyIndicator = new BusyIndicator()) {
+        if (this._derivedCommand) {
+            this.extendHelpDocs(this.getAllDependencies(currentWorkingDirectory, coreLanguage, commandArguments, commandOptions, namespace), 
+                                this.usage, this.help);
+            if (hasHelpOption(commandOptions)) {
+                outputter.print(this.helpDocs);
+                return;
+            }
+            await this._derivedCommand.action(dependencyResolvers, currentWorkingDirectory, coreLanguage, commandArguments, commandOptions, namespace, outputter, busyIndicator)
+        }
+    }
+
+    getAllDependencies(currentWorkingDirectory: string, coreLanguage: string, commandArguments?: string[], commandOptions?: Map<string, any>, namespace?: string) {
+        return this._derivedCommand? this._derivedCommand.getAllDependencies(currentWorkingDirectory, coreLanguage, commandArguments, commandOptions, namespace) : this.dependencies;
+    }
+
+    /**
+     * Gets the message that should be printed when help is needed for a command
+     *
+     * @readonly
+     * @memberof Command
+     */
+    get helpDocs() {
+        let res = [chalk.bold('Usage:'), `\t${this.usage}`];
+        res.push('', this.description);
+        if (this.help) res.push('', chalk.bold('Help:'), this.help);
+        
+        return res.join('\n');
+    }
+
+    /**
+     * Extends the help docs with the given dependencies
+     */
+    extendHelpDocs(dependencies: IDependency[], usagePrefix?: string, helpPrefix?: string) {
+        let argumentDependencies: IPromptDependency[] = dependencies.filter(_ => dependencyIsPromptDependency(_) && _.userInputType === argumentUserInputType) as IPromptDependency[];
+        const usageText = argumentDependencies.map(_ => _.optional? `[--${_.name}]`: `<${_.name}>`).join(' ');
+        const helpText = argumentDependencies.map(_ => `\t${_.name}: ${_.description}`).join('\n');
+
+        this.usage = usagePrefix? `${usagePrefix} ${usageText}` : usageText;
+        this.help = helpPrefix? `${helpPrefix}\n${helpText}` : helpText;
+    }
+}
---
+++ Source/Commands/Namespace.ts
@@ -0,0 +1,81 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { IBusyIndicator, ICanOutputMessages } from '@dolittle/tooling.common.utilities';
+import chalk from 'chalk';
+import { Command } from './Command';
+import { Outputter } from '../Outputter';
+import { BusyIndicator } from '../BusyIndicator';
+import { CommandGroup } from './CommandGroup';
+import { INamespace } from '@dolittle/tooling.common.commands';
+import { IDependencyResolvers } from '@dolittle/tooling.common.dependencies';
+
+/**
+ * Base class for {Namespace} commands
+ *
+ * @export
+ * @class Namespace
+ * @extends {Command}
+ */
+export class Namespace extends Command {
+    
+    readonly commands: Command[] = [];
+    readonly commandGroups: CommandGroup[] = [];
+
+    static fromNamespace(namespace: INamespace, commands: Command[], commandGroups: CommandGroup[]) {
+        const usage = `dolittle ${namespace.name} <command-group> <command>`;
+        return new Namespace(namespace.name, commands, commandGroups, namespace.description, usage, undefined, namespace.shortDescription);
+    }
+
+    /**
+     * Instantiates an instance of {Namespace}.
+     * @param {string} name
+     * @param {Command[]} commands
+     * @param {CommandGroup[]} commandGroups
+     * @param {string} description
+     * @param {string} usage
+     * @param {string} [help]
+     * @param {string} [shortDescription]
+     */
+    constructor(name: string, commands: Command[], commandGroups: CommandGroup[], description: string, usage: string, help?: string, shortDescription?: string) {
+        super(name, description, usage, undefined, help, shortDescription);
+        this.commands = commands;
+        this.commandGroups = commandGroups;
+        
+    }
+
+    get helpDocs() {
+        let res = [chalk.bold('Usage:'), `\t${this.usage}`];
+        res.push('', this.description);
+        if (this.commands.length > 0) res.push('', chalk.bold('Commands:'), this.commands.map(cmd => `\t${chalk.bold(cmd.name)} - ${cmd.shortDescription}`).join('\n'));
+        if (this.commandGroups.length > 0) res.push('', chalk.bold('Command groups:'), this.commandGroups.map(cmd => `\t${chalk.bold(cmd.name)} - ${cmd.shortDescription}`).join('\n'));
+        if (this.help) res.push('', chalk.bold('Help:'), this.help);
+        
+        return res.join('\n');
+
+    }
+    async action(dependencyResolvers: IDependencyResolvers, currentWorkingDirectory: string, coreLanguage: string, commandArguments: string[], commandOptions: Map<string, any>, namespace?: string, outputter: ICanOutputMessages = new Outputter(), busyIndicator: IBusyIndicator = new BusyIndicator()) {
+        let firstArgument = commandArguments[0];
+        if (!firstArgument || firstArgument === '') {
+            outputter.print(this.helpDocs);
+            return;
+        }
+
+        const isCommandGroup = this.commandGroups.map(_ => _.name).includes(firstArgument);
+        const isBasicCommand = this.commands.map(_ => _.name).includes(firstArgument);
+        let command: Command | undefined
+        if (isBasicCommand) command = this.commands.find(_ => _.name === firstArgument);
+        else if (isCommandGroup) command = this.commandGroups.find(_ => _.name === firstArgument);
+
+        if (command) {
+            commandArguments.shift();
+            await command.action(dependencyResolvers, currentWorkingDirectory, coreLanguage, commandArguments, commandOptions, this.name, outputter, busyIndicator);
+        }
+        else {
+            outputter.warn(`No such sub command as '${firstArgument}'`);
+            outputter.print();
+            outputter.print(this.helpDocs);
+        }
+    }
+}
